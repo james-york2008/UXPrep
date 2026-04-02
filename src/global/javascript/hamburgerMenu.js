3cde77de6158b08c4e0c25b4a76b2d3083eaea99
James <jwyork2008@gmail.com>
2026-04-02 11:28:56 -0500

    Replaced js altering styles in hamburgerMenu.js with css

--- src/global/javascript/hamburgerMenu.js
+++ src/global/javascript/hamburgerMenu.js
@@ -7,12 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
         navList.classList.toggle('d-none')
         navList.classList.toggle('d-show')
 
-        if (navList.classList.contains('d-show')) {
-            navList.style.padding = '2rem'
-        } else {
-            navList.style.padding = '0'
-        }
-
         const expanded = btn.getAttribute('aria-expanded') === 'true'
         btn.setAttribute('aria-expanded', (!expanded).toString())
     })
