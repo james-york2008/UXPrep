e5e4d25634758ac479eec462521fcf5a8041ba96
James <jwyork2008@gmail.com>
2026-06-13 14:05:44 -0500

    Designed hero section

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/navbar/Navbar"
+import Hero from "./hero/Hero"
 import Legal from "../../components/legal/Legal"
 
 export default function LandingPage () {
   return(
     <>
       <Navbar />
+      <Hero />
 
       <Legal />
     </>
