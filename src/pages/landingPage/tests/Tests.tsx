0292423f80237fdc5c0a35e6d10fa81d7e8e05c6
James <jwyork2008@gmail.com>
2026-07-09 19:50:51 -0500

    Redesigned the landing page to use JSON data

--- src/pages/landingPage/tests/Tests.tsx
+++ src/pages/landingPage/tests/Tests.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import styles from '../../../css/landingPage/Tests.module.css'
-import { landingPageTests } from '../../../data/landingPageTests'
+import { useEffect, useState } from 'react'
+import type { LandingPageTest } from '../../../types/landingPage/LandingPageTest'
 
 const testIcons: Record<string, string> = import.meta.glob(
   '../../../assets/testIcons/*',
@@ -11,12 +12,40 @@ const testIcons: Record<string, string> = import.meta.glob(
 )
 
 export default function Tests () {
+  const [landingPageTests, setLandingPageTests] = useState<LandingPageTest[]>()
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const res = await fetch('/UXPrep/data/landingPageTests.json')
+      
+        if (!res.ok) {
+          throw new Error(`HTTP ${res.status}`)
+        }
+  
+        let data = await res.json()
+        setLandingPageTests(data)
+      } catch (err) {
+        console.error(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  if (loading) (
+    <p>Loading quizzes</p>
+  )
+  
   return(
     <section className={styles.tests} id='tests'>
       <h2>Choose a quiz topic</h2>
 
       <ul className={styles.testsList}>
-        {landingPageTests.map((test) => {
+        {landingPageTests?.map((test) => {
           let testIcon = testIcons[`../../../assets/testIcons/${test.imageLink}`]
 
           return(
